301c6f4f7f64e54a8f7b2cc841b8dc75bba6f27b
Sithum Dissanayake <contact@sithumd.com>
2025-10-10 22:27:46 +1100

    [main] 10-10 - 1

---
+++ input/HPL/Sithum/10-10 - 1 2/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=hpl-test       # Job name
+#SBATCH --ntasks=192              # Total MPI tasks
+#SBATCH --ntasks-per-node=64       # MPI tasks per node
+#SBATCH --cpus-per-task=1         # CPU cores per MPI task
+#SBATCH --time=00:10:00           # Time limit hh:mm:ss
+#SBATCH --nodes=3                 # Number of nodes
+
+export PATH=/opt/slurm/bin:/usr/bin:/bin:/opt/openmpi-4.1.6/bin:$PATH
+export LD_LIBRARY_PATH=/opt/openmpi-4.1.6/lib:$LD_LIBRARY_PATH
+
+# Ulimits
+ulimit -l unlimited
+ulimit -n 65536
+
+# Run the MPI program
+mpirun ./xhpl
