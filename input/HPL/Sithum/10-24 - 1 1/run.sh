e1b2b7cee78e6d513683ae1eee334bc6075154e3
Sithum Dissanayake <contact@sithumd.com>
2025-10-24 11:36:53 +1100

    [main] 10-24 - 1

---
+++ input/HPL/Sithum/10-24 - 1 1/run.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#SBATCH --job-name=hpl-test       # Job name
+#SBATCH --ntasks=256              # Total MPI tasks
+#SBATCH --ntasks-per-node=64       # MPI tasks per node
+#SBATCH --cpus-per-task=1         # CPU cores per MPI task
+#SBATCH --time=24:00:00           # Time limit hh:mm:ss
+#SBATCH --nodes=4                 # Number of nodes
+
+spack load hpl %aocc
+
+unset OMPI_MCA_osc
+
+export PATH=/opt/openmpi-4.1.6/bin:$PATH
+export LD_LIBRARY_PATH=/opt/openmpi-4.1.6/lib:$LD_LIBRARY_PATH
+
+# MPI settings (Ethernet)
+export OMPI_MCA_btl=self,vader,tcp
+export OMPI_MCA_btl_tcp_if_include=enp1s0
+export OMPI_MCA_oob_tcp_if_include=enp1s0
+export OMPI_MCA_pml=ob1
+
+# Collective tuning
+export OMPI_MCA_coll_tuned_use_dynamic_rules=1
+export OMPI_MCA_coll_tuned_bcast_algorithm=4
+export OMPI_MCA_coll_tuned_allreduce_algorithm=6
+
+# SLURM Integration
+export OMPI_MCA_plm=slurm
+export OMPI_MCA_orte_launch=slurm
+
+# Thread binding and OpenMP
+export OMP_NUM_THREADS=1
+export OMP_PROC_BIND=close
+export OMP_PLACES=cores
+
+# amdblis (BLAS layer) optimizations
+export BLIS_JC_NT=1  # (No outer loop parallelization)
+export BLIS_IC_NT=$OMP_NUM_THREADS # (# of 2nd level threads – one per core in the shared L3 cache domain):
+export BLIS_JR_NT=1 # (No 4th level threads)
+export BLIS_IR_NT=1 # (No 5th level threads)
+
+# Memory and file limits
+ulimit -l unlimited
+ulimit -n 65536
+
+# Flush any pending writes
+sync
+
+# Drop caches
+echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null
+
+# Forces memory compaction
+echo 1 > /proc/sys/vm/compact_memory
+
+# Run the MPI program
+mpirun Xhpl
